85edaf97927c413778d6d988a0444db3b6cd33ea
Sumant Rana <sumantrana@rsumantsing-a01.vmware.com>
2020-12-24 18:47:51 +0800

    Initial Commit

---
+++ src/test/java/com/sumant/springbootlearning/spring301/Spring301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumant.springbootlearning.spring301;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring301ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
